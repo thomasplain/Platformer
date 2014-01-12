6be5be7b8b308458858603dd14a4c2de3b37006a
Tom Gillooly <tomgillooly@gmail.com>
2014-01-12 21:03:45 +1300

    Jump force injected as dependency

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -10,7 +10,6 @@ public class playerMovement : MonoBehaviour {
 	private State playerState;
 	static private GameObject player;
 
-	static public float _jumpForce;
 	public float jumpForce = 1000f;
 	public float maxSpeed = 15f;
 
@@ -18,7 +17,6 @@ public class playerMovement : MonoBehaviour {
 	{
 		groundCheck = GameObject.FindGameObjectWithTag ("groundCheck");
 		player = GameObject.FindGameObjectWithTag ("Player");
-		_jumpForce = jumpForce;
 		playerState = new Grounded (player, groundCheck, jumpForce);
 	}
 
@@ -31,15 +29,5 @@ public class playerMovement : MonoBehaviour {
 		Vector3 horizontalVelocity = Vector3.right * h * maxSpeed * Time.deltaTime; 
 
 		transform.position += horizontalVelocity;	
-	}
-
-	static public GameObject getPlayer()
-	{
-		return player;
-	}
-
-	static public GameObject getGroundCheck()
-	{
-		return groundCheck;
 	}
 }
